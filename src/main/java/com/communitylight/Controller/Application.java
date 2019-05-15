659ce0146a06ab00fe07026f52a04545556e0597
SEPTEMBERx <527392111@QQ.COM>
2019-05-15 11:29:53 +0800

    init repo

---
+++ src/main/java/com/communitylight/Controller/Application.java
@@ -0,0 +1,18 @@
+package com.communitylight.Controller;
+
+/**
+ * @Author: Septemberx
+ * @Date: 2019/5/15 10:28
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
